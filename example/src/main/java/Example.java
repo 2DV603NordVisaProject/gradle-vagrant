61e38ed5f44ccf8d651953ce643dc8ebff91e4be
Axel Nilsson <an222yp@student.lnu.se>
2016-11-14 23:59:09 +0100

    Started on an example to test gradle

---
+++ example/src/main/java/Example.java
@@ -0,0 +1,22 @@
+/**
+ * Class Example
+ *
+ * @author Axel Nilsson (axnion)
+ */
+public class Example {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        return a / b;
+    }
+}
