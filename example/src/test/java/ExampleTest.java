b03183e6140640d59944694e514801fc5814d159
Axel Nilsson <an222yp@student.lnu.se>
2016-11-15 00:44:53 +0100

    Created simple example for testing

--- example/src/test/java/ExampleTest.java
+++ example/src/test/java/ExampleTest.java
@@ -1,8 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  * Class ExampleTest
  *
  * @author Axel Nilsson (axnion)
  */
 public class ExampleTest {
-    
+    private Example target;
+
+    @Before
+    public void createExample() {
+        target = new Example();
+    }
+
+    @Test
+    public void addTest() {
+        assertEquals(2, target.add(1, 1));
+    }
+
+    @Test
+    public void subTest() {
+        assertEquals(0, target.sub(1, 1));
+    }
+
+    @Test
+    public void multiplyTest() {
+        assertEquals(1, target.multiply(1, 1));
+    }
+
+    @Test
+    public void divideTest() {
+        assertEquals(1.0, target.divide(1, 1));
+    }
+
+    @Test
+    public void divideByZeroTest() {
+        target.divide(1, 0);
+    }
 }
